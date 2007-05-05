64bef39c398dc47105f9eced881db9c94fb01da3
Chaos User <chaos@cc.gatech.edu>
2007-05-05 00:20:56 +0000

    normal version update
    git-svn-id: http://svn.research.cc.gatech.edu/kaos/dill/trunk@2923 8ba6a8b9-845b-0410-94f0-974be2f5df47

--- version.c
+++ version.c
@@ -11,7 +11,7 @@
 #include <stdio.h>
 #include "config.h"
 
-static char *DILL_version = "DILL Version 1.0.12 -- Fri May  4 20:11:13 EDT 2007\n";
+static char *DILL_version = "DILL Version 1.0.13 -- Fri May  4 20:20:56 EDT 2007\n";
 
 void DILLprint_version(){
     printf("%s",DILL_version);
