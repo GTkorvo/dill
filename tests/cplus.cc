5fa12acf130fbaf86b4932a860f1430112e8350a
Chaos User <chaos@cc.gatech.edu>
2007-09-14 14:49:14 +0000

    tweak arg parsing
    git-svn-id: http://svn.research.cc.gatech.edu/kaos/dill/trunk@2962 8ba6a8b9-845b-0410-94f0-974be2f5df47

--- tests/cplus.cc
+++ tests/cplus.cc
@@ -109,10 +109,12 @@ int main(int argc, char **argv)
   int no_dcg = 0;
   int result;
   for (int i=1; i < argc; i++) {
-    if (strcmp(argv[i], "-no_dcg") == 0) {
+    if (argv[i] == "-no_dcg") {
       no_dcg++;
-    } else if (strcmp(argv[i], "-v") == 0) {
+    } else if (argv[i] =="-v") {
       verbose++;
+    } else {
+      std::cout << "Unknown arguent \"" << argv[i] << "\"" << std::endl;
     }
   }
 
