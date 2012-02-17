4e47e4a06a7964b8ce5d81d4861cfd3f303eb629
Greg Eisenhauer <eisen@cc.gatech.edu>
2012-02-17 20:49:48 +0000

    Pieces of a CMake conversion.  Still in progress.
    git-svn-id: http://svn.research.cc.gatech.edu/kaos/dill/trunk@10738 8ba6a8b9-845b-0410-94f0-974be2f5df47

---
+++ cmake/FindBinUtils.cmake
@@ -0,0 +1,34 @@
+#
+#  BINUTILS_FOUND - system has the BinUtils library BINUTILS_INCLUDE_DIR - the BinUtils include directory BINUTILS_LIBRARIES - The libraries needed
+#  to use BinUtils
+IF (NOT BINUTILS_FOUND)
+  IF (NOT ${EXTERNALS_DIRECTORY} STREQUAL "")
+    IF (${ARCH64BIT} EQUAL 1)
+      SET (osdir "linux64_gcc4.1.1")
+    ELSE()
+      SET (osdir "linux32_gcc4.1.1")
+    ENDIF()
+    FIND_PATH (BINUTILS_INCLUDE_DIR NAMES bfd.h PATHS "${EXTERNALS_DIRECTORY}/binutils/${osdir}/include" NO_DEFAULT_PATH)
+    FIND_LIBRARY (BINUTILS_LIBRARIES NAMES bfd PATHS "${EXTERNALS_DIRECTORY}/binutils/${osdir}/lib" NO_DEFAULT_PATH)
+    # We also need libiberty - but that is complicated by the fact that some distros ship libibery_pic that you have to use in .so's, while on others libiberty.a is PIC-friendly
+    FIND_LIBRARY (IBERTY_LIBRARIES NAMES iberty_pic PATHS "${EXTERNALS_DIRECTORY}/binutils/${osdir}/lib" NO_DEFAULT_PATH)
+    FIND_LIBRARY (IBERTY_LIBRARIES NAMES iberty PATHS "${EXTERNALS_DIRECTORY}/binutils/${osdir}/lib" NO_DEFAULT_PATH)
+  ENDIF()
+  # if we didn't find in externals, look in system include path
+  if (USE_NATIVE_LIBRARIES)
+    FIND_PATH (BINUTILS_INCLUDE_DIR NAMES bfd.h)
+    FIND_LIBRARY (BINUTILS_LIBRARIES NAMES bfd)
+    FIND_LIBRARY (IBERTY_LIBRARIES NAMES iberty_pic)
+    FIND_LIBRARY (IBERTY_LIBRARIES NAMES iberty)
+  endif()
+  include(FindPackageHandleStandardArgs)
+  find_package_handle_standard_args(BinUtils DEFAULT_MSG
+    BINUTILS_LIBRARIES
+    BINUTILS_INCLUDE_DIR
+    IBERTY_LIBRARIES
+  )
+  IF (BINUTILS_FOUND)
+    set (BINUTILS_LIBRARIES ${BINUTILS_LIBRARIES} ${IBERTY_LIBRARIES} )
+  ENDIF()
+  MARK_AS_ADVANCED(BINUTILS_INCLUDE_DIR BINUTILS_LIBRARIES)
+ENDIF()
