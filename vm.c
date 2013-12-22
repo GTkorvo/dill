f4e4d95c05c8746229bc2cf828a83c36919c2b70
Greg Eisenhauer <eisen@cc.gatech.edu>
2013-12-22 14:18:04 +0000

    Half of the vtests test working under libffi emulation.  Prior emulation was pretty much broken, so I'll go ahead and commit this update.  Modify CMakeLists.txt to find libffi and setup for emulation.  Various changes in virtual.ops and vm.c both to use libffi rather than the avcall/ffcall and to fix bugs in the original emulation code.
    git-svn-id: http://svn.research.cc.gatech.edu/kaos/dill/trunk@16603 8ba6a8b9-845b-0410-94f0-974be2f5df47

--- vm.c
+++ vm.c
@@ -15,130 +15,104 @@
 
 static void run_emulation(dill_exec_ctx ec);
 
-#define EMULATION_BODY \
-    va_list ap;\
-    int i;\
-    int param_count = ec->dc->p->save_param_count;\
-    ec->p = malloc(param_count * sizeof(struct reg_type));\
-\
-    va_start(ap, ec);\
-    if ((param_count < 1) || (ec->dc->p->c_param_args[0].type != DILL_EC)) {\
-	fprintf(stderr, "No emulation possible, %%EC not first argument\n");\
-    }\
-    ec->p[0].u.p.p = ec;\
-    for (i= 1; i < param_count; i++) {\
-	switch (ec->dc->p->c_param_args[i].type) {\
-	case DILL_C:\
-	    ec->p[i].u.c.c = va_arg(ap, int);\
-	    break;\
-	case DILL_UC:\
-	    ec->p[i].u.uc.uc = va_arg(ap, int);\
-	    break;\
-	case DILL_S:\
-	    ec->p[i].u.s.s = va_arg(ap, int);\
-	    break;\
-	case DILL_US:\
-	    ec->p[i].u.us.us = va_arg(ap, int);\
-	    break;\
-	case DILL_I:\
-	    ec->p[i].u.i.i = va_arg(ap, int);\
-	    break;\
-	case DILL_U:\
-	    ec->p[i].u.u.u = va_arg(ap, unsigned);\
-	    break;\
-	case DILL_L:\
-	    ec->p[i].u.l.l = va_arg(ap, long);\
-	    break;\
-	case DILL_UL:\
-	    ec->p[i].u.ul.ul = va_arg(ap, unsigned long);\
-	    break;\
-	case DILL_F:\
-	    ec->p[i].u.f.f = va_arg(ap, double);\
-	    break;\
-	case DILL_D:\
-	    ec->p[i].u.d.d = va_arg(ap, double);\
-	    break;\
-	case DILL_P:\
-	case DILL_EC:\
-	    ec->p[i].u.p.p = va_arg(ap, char*);\
-	    break;\
-default: printf("Unknown param type %d\n",ec->dc->p->c_param_args[i].type);\
-	}\
-    }\
-    va_end(ap);\
-\
+#include "vm_include.c"
+#ifdef BUILD_EMULATOR
+#include <ffi.h>
+#endif
+
+static
+void emu_func(ffi_cif *cif, void*vret, void* args[], void *client_data)
+{
+    int i;
+    dill_stream c = client_data;
+    int param_count = c->p->save_param_count;
+    dill_exec_ctx ec;
+
+    if ((param_count >= 1) && (c->p->c_param_args[0].type == DILL_EC)) {
+	ec = *(dill_exec_ctx*)args[0];
+    } else {
+	/* create an execution context to use */
+	ec = dill_get_exec_context(c);
+    }	
+    if (!ec->p) {
+	ec->p = malloc(param_count * sizeof(struct reg_type));
+    }
+    for (i= 1; i < param_count; i++) {
+	switch (c->p->c_param_args[i].type) {
+	case DILL_C:
+	    ec->p[i].u.c.c = *((char **)args)[i];
+	    break;
+	case DILL_UC:
+	    ec->p[i].u.uc.uc = *((unsigned char **)args)[i];
+	    break;
+	case DILL_S:
+	    ec->p[i].u.s.s = *((short **)args)[i];
+	    break;
+	case DILL_US:
+	    ec->p[i].u.us.us = *((unsigned short **)args)[i];
+	    break;
+	case DILL_I:
+	    ec->p[i].u.i.i = *((int **)args)[i];
+	    break;
+	case DILL_U:
+	    ec->p[i].u.u.u = *((unsigned int **)args)[i];
+	    break;
+	case DILL_L:
+	    ec->p[i].u.l.l = *((long **)args)[i];
+	    break;
+	case DILL_UL:
+	    ec->p[i].u.ul.ul = *((unsigned long **)args)[i];
+	    break;
+	case DILL_F:
+	    ec->p[i].u.f.f = *((float **)args)[i];
+	    break;
+	case DILL_D:
+	    ec->p[i].u.d.d = *((double **)args)[i];
+	    break;
+	case DILL_P:
+	case DILL_EC:
+	    ec->p[i].u.p.p = *((void ***)args)[i];
+	    break;
+	}
+    }
+    /* execute the function */
     run_emulation(ec);
-
-int
-emulate_vm_i_func(dill_exec_ctx ec, ...)
-{
-    EMULATION_BODY
-    return (PREG(ec, ec->ret_reg)->u.i.i);
-}
-
-char emulate_vm_c_func(dill_exec_ctx ec, ...)
-{
-    EMULATION_BODY
-    return (PREG(ec, ec->ret_reg)->u.u.u);
-}
-
-unsigned char emulate_vm_uc_func(dill_exec_ctx ec, ...)
-{
-    EMULATION_BODY
-    return (PREG(ec, ec->ret_reg)->u.uc.uc);
-}
-
-short emulate_vm_s_func(dill_exec_ctx ec, ...)
-{
-    EMULATION_BODY
-    return (PREG(ec, ec->ret_reg)->u.s.s);
-}
-
-unsigned short emulate_vm_us_func(dill_exec_ctx ec, ...)
-{
-    EMULATION_BODY
-    return (PREG(ec, ec->ret_reg)->u.us.us);
-}
-
-unsigned int emulate_vm_u_func(dill_exec_ctx ec, ...)
-{
-    EMULATION_BODY
-    return (PREG(ec, ec->ret_reg)->u.u.u);
-}
-
-long emulate_vm_l_func(dill_exec_ctx ec, ...)
-{
-    EMULATION_BODY
-    return (PREG(ec, ec->ret_reg)->u.l.l);
-}
-
-unsigned long emulate_vm_ul_func(dill_exec_ctx ec, ...)
-{
-    EMULATION_BODY
-    return (PREG(ec, ec->ret_reg)->u.ul.ul);
-}
-
-void *emulate_vm_p_func(dill_exec_ctx ec, ...)
-{
-    EMULATION_BODY
-    return (PREG(ec, ec->ret_reg)->u.p.p);
-}
-
-float emulate_vm_f_func(dill_exec_ctx ec, ...)
-{
-    EMULATION_BODY
-    return (PREG(ec, ec->ret_reg)->u.f.f);
-}
-
-double emulate_vm_d_func(dill_exec_ctx ec, ...)
-{
-    EMULATION_BODY
-    return (PREG(ec, ec->ret_reg)->u.d.d);
-}
-
-void emulate_vm_v_func(dill_exec_ctx ec, ...) 
-{
-    EMULATION_BODY
+    /* do return */
+    switch(c->p->ret_type) {
+    case DILL_C:
+	*((char *)vret) = (PREG(ec, ec->ret_reg)->u.c.c);
+	break;
+    case DILL_UC:
+	*((unsigned char *)vret) = (PREG(ec, ec->ret_reg)->u.uc.uc);
+	break;
+    case DILL_S:
+	*((short *)vret) = (PREG(ec, ec->ret_reg)->u.s.s);
+	break;
+    case DILL_US:
+	*((unsigned short *)vret) = (PREG(ec, ec->ret_reg)->u.us.us);
+	break;
+    case DILL_I:
+	*((int *)vret) = (PREG(ec, ec->ret_reg)->u.i.i);
+	break;
+    case DILL_U:
+	*((unsigned int *)vret) = (PREG(ec, ec->ret_reg)->u.u.u);
+	break;
+    case DILL_L:
+	*((long *)vret) = (PREG(ec, ec->ret_reg)->u.l.l);
+	break;
+    case DILL_UL:
+	*((unsigned long *)vret) = (PREG(ec, ec->ret_reg)->u.ul.ul);
+	break;
+    case DILL_F:
+	*((float *)vret) = (PREG(ec, ec->ret_reg)->u.f.f);
+	break;
+    case DILL_D:
+	*((double *)vret) = (PREG(ec, ec->ret_reg)->u.d.d);
+	break;
+    case DILL_P:
+	*((void**)vret) = (PREG(ec, ec->ret_reg)->u.p.p);
+	break;
+    }
 }
 
 extern void *
@@ -150,66 +124,121 @@ int available_size;
     return c->p->fp;
 }
 
-static void
-print_warning()
-{
-    fprintf(stderr, "No emulation possible, %%EC not first argument\n");
-}
+     /* Acts like puts with the file given at time of enclosure. */
+     void puts_binding(ffi_cif *cif, unsigned int *ret, void* args[],
+                       FILE *stream)
+     {
+	 printf("We're in the puts binding, args %p, %d, %d\n", *((char***)args)[0], *((int**)args)[1], *((int**)args)[2]);
+     }
 
 void
 setup_VM_proc(dill_stream c)
 {
-    if ((c->p->c_param_count < 1) || (c->p->c_param_args[0].type != DILL_EC)) {
-	fprintf(stderr, "No emulation possible, %%EC not first argument\n");
-	c->p->fp = (void*)print_warning;
-	return;
-    }
+     ffi_cif *cifp = malloc(sizeof(ffi_cif));
+    ffi_type **args = NULL;
+    ffi_closure *closure;
+    void *func;
+    ffi_type *ret_type;
+    void *ret_addr = NULL;
+    int i;
+
     c->p->fp = NULL;
     c->j->clone_code = emulate_clone_code;
+    /* Allocate closure and func */
+    closure = ffi_closure_alloc(sizeof(ffi_closure), &func);
+
     switch (c->p->ret_type) {
     case DILL_C:
-	c->p->fp = (void*)emulate_vm_c_func;
+	ret_type = &ffi_type_sint8;
 	break;
     case DILL_UC:
-	c->p->fp = (void*)emulate_vm_uc_func;
+	ret_type = &ffi_type_uint8;
 	break;
     case DILL_S:
-	c->p->fp = (void*)emulate_vm_s_func;
+	ret_type = &ffi_type_sint16;
 	break;
     case DILL_US:
-	c->p->fp = (void*)emulate_vm_us_func;
+	ret_type = &ffi_type_uint16;
 	break;
     case DILL_I:
-	c->p->fp = (void*)emulate_vm_i_func;
+	ret_type = &ffi_type_sint32;
 	break;
     case DILL_U:
-	c->p->fp = (void*)emulate_vm_u_func;
+	ret_type = &ffi_type_uint32;
 	break;
     case DILL_L:
-	c->p->fp = (void*)emulate_vm_l_func;
+	ret_type = &ffi_type_sint64;
 	break;
     case DILL_UL:
-	c->p->fp = (void*)emulate_vm_ul_func;
+	ret_type = &ffi_type_uint64;
 	break;
     case DILL_P:
-	c->p->fp = (void*)emulate_vm_p_func;
+	ret_type = &ffi_type_pointer;
 	break;
     case DILL_F:
-	c->p->fp = (void*)emulate_vm_f_func;
+	ret_type = &ffi_type_float;
 	break;
     case DILL_D:
-	c->p->fp = (void*)emulate_vm_d_func;
+	ret_type = &ffi_type_double;
 	break;
     case DILL_V:
-	c->p->fp = (void*)emulate_vm_v_func;
+	ret_type = &ffi_type_void;
 	break;
     }
+    args = malloc(c->p->c_param_count * sizeof(args[0]));
+
+    for (i=0; i < c->p->c_param_count; i++) {
+	switch(c->p->c_param_args[i].type) {
+	case DILL_C:
+	    args[i] = &ffi_type_sint8;
+	    break;
+	case DILL_UC:
+	    args[i] = &ffi_type_uint8;
+	    break;
+	case DILL_S:
+	    args[i] = &ffi_type_sint16;
+	    break;
+	case DILL_US:
+	    args[i] = &ffi_type_uint16;
+	    break;
+	case DILL_I:
+	    args[i] = &ffi_type_sint32;
+	    break;
+	case DILL_U:
+	    args[i] = &ffi_type_uint32;
+	    break;
+	case DILL_L:
+	    args[i] = &ffi_type_sint64;
+	    break;
+	case DILL_UL:
+	    args[i] = &ffi_type_uint64;
+	    break;
+	case DILL_EC:
+	case DILL_P:
+	    args[i] = &ffi_type_pointer;
+	    break;
+	case DILL_F:
+	    args[i] = &ffi_type_float;
+	    break;
+	case DILL_D:
+	    args[i] = &ffi_type_double;
+	    break;
+	case DILL_V:
+	    args[i] = &ffi_type_void;
+	}
+    }
+    if (!ffi_prep_cif(cifp, FFI_DEFAULT_ABI, c->p->c_param_count,
+		      ret_type, args) == FFI_OK) {
+	return;
+    }
+    if (!ffi_prep_closure_loc(closure, cifp, emu_func,
+			      c, func) == FFI_OK) {
+
+	return;
+    }
+    c->p->fp = func;
 }
 
-#include "vm_include.c"
-#ifdef BUILD_EMULATOR
-#include <avcall.h>
-#endif
 static void run_emulation(dill_exec_ctx ec)
 {
     dill_stream c = ec->dc;
@@ -330,11 +359,12 @@ static void run_emulation(dill_exec_ctx ec)
 	    pdest->u = pused[0]->u;
 	    break;
 	}
-	case iclass_reti:
+	case iclass_reti: 
 	    /* return immediate integer insns */
 	    /* arbitrarily destroy reg 100 and return it */
 	    ec->ret_reg = 100;
-	    PREG(ec, ec->ret_reg)->u.i.i = ip->opnds.a3i.u.imm;
+//	    switch(ip->insn_code & 0xf) {
+	    PREG(ec, ec->ret_reg)->u.l.l = ip->opnds.a3i.u.imm;
 	    return;
 	case iclass_branch:
 	{
@@ -384,8 +414,12 @@ static void run_emulation(dill_exec_ctx ec)
 	    int ret_reg = ip->opnds.bri.src;
 	    int reg = ip->insn_code & 0x10;
 	    int typ = ip->insn_code & 0xf;
+	    ffi_type **args;
+	    void **values;
 	    void *func;
-	    av_alist alist;
+	    ffi_type *ret_type;
+	    void *ret_addr = NULL;
+	    ffi_cif cif;
 	    pused[0] = PREG(ec, ret_reg);
 	    if (reg != 0) {
 		func = PREG(ec, (long)imm)->u.p.p;
@@ -394,82 +428,108 @@ static void run_emulation(dill_exec_ctx ec)
 	    }
 	    switch(typ) {
 	    case DILL_C:
-		av_start_char(alist, func, &pused[0]->u.c.c);
+		ret_type = &ffi_type_sint8;
+		ret_addr = &pused[0]->u.c.c;
 		break;
 	    case DILL_UC:
-		av_start_uchar(alist, func, &pused[0]->u.uc.uc);
+		ret_type = &ffi_type_uint8;
+		ret_addr = &pused[0]->u.uc.uc;
 		break;
 	    case DILL_S:
-		av_start_short(alist, func, &pused[0]->u.s.s);
+		ret_type = &ffi_type_sint16;
+		ret_addr = &pused[0]->u.s.s;
 		break;
 	    case DILL_US:
-		av_start_ushort(alist, func, &pused[0]->u.us.us);
+		ret_type = &ffi_type_uint16;
+		ret_addr = &pused[0]->u.us.us;
 		break;
 	    case DILL_I:
-		av_start_int(alist, func, &pused[0]->u.i.i);
+		ret_type = &ffi_type_sint32;
+		ret_addr = &pused[0]->u.i.i;
 		break;
 	    case DILL_U:
-		av_start_uint(alist, func, &pused[0]->u.u.u);
+		ret_type = &ffi_type_uint32;
+		ret_addr = &pused[0]->u.u.u;
 		break;
 	    case DILL_L:
-		av_start_long(alist, func, &pused[0]->u.l.l);
+		ret_type = &ffi_type_sint64;
+		ret_addr = &pused[0]->u.l.l;
 		break;
 	    case DILL_UL:
-		av_start_ulong(alist, func, &pused[0]->u.ul.ul);
+		ret_type = &ffi_type_uint64;
+		ret_addr = &pused[0]->u.ul.ul;
 		break;
 	    case DILL_P:
-		av_start_ptr(alist, func, void*, &pused[0]->u.p.p);
+		ret_type = &ffi_type_pointer;
+		ret_addr = &pused[0]->u.p.p;
 		break;
 	    case DILL_F:
-		av_start_float(alist, func, &pused[0]->u.f.f);
+		ret_type = &ffi_type_float;
+		ret_addr = &pused[0]->u.f.f;
 		break;
 	    case DILL_D:
-		av_start_double(alist, func, &pused[0]->u.d.d);
+		ret_type = &ffi_type_double;
+		ret_addr = &pused[0]->u.d.d;
 		break;
 	    case DILL_V:
-		av_start_void(alist, func);
+		ret_type = &ffi_type_void;
+		ret_addr = NULL;
 		break;
 	    }
 	    for (i=0; i < ec->out_param_count; i++) {
 		switch(ec->out_params[i].typ) {
 		case DILL_C:
-		    av_char(alist, ec->out_params[i].val.u.c.c);
+		    args[i] = &ffi_type_sint8;
+		    values[i] = &ec->out_params[i].val.u.c.c;
 		    break;
 		case DILL_UC:
-		    av_uchar(alist, ec->out_params[i].val.u.uc.uc);
+		    args[i] = &ffi_type_uint8;
+		    values[i] = &ec->out_params[i].val.u.uc.uc;
 		    break;
 		case DILL_S:
-		    av_short(alist, ec->out_params[i].val.u.s.s);
+		    args[i] = &ffi_type_sint16;
+		    values[i] = &ec->out_params[i].val.u.s.s;
 		    break;
 		case DILL_US:
-		    av_ushort(alist, ec->out_params[i].val.u.us.us);
+		    args[i] = &ffi_type_uint16;
+		    values[i] = &ec->out_params[i].val.u.us.us;
 		    break;
 		case DILL_I:
-		    av_int(alist, ec->out_params[i].val.u.i.i);
+		    args[i] = &ffi_type_sint32;
+		    values[i] = &ec->out_params[i].val.u.i.i;
 		    break;
 		case DILL_U:
-		    av_uint(alist, ec->out_params[i].val.u.u.u);
+		    args[i] = &ffi_type_uint32;
+		    values[i] = &ec->out_params[i].val.u.u.u;
 		    break;
 		case DILL_L:
-		    av_long(alist, ec->out_params[i].val.u.l.l);
+		    args[i] = &ffi_type_sint64;
+		    values[i] = &ec->out_params[i].val.u.l.l;
 		    break;
 		case DILL_UL:
-		    av_ulong(alist, ec->out_params[i].val.u.ul.ul);
+		    args[i] = &ffi_type_uint64;
+		    values[i] = &ec->out_params[i].val.u.ul.ul;
 		    break;
 		case DILL_P:
-		    av_ptr(alist, void*, ec->out_params[i].val.u.p.p);
+		    args[i] = &ffi_type_pointer;
+		    values[i] = &ec->out_params[i].val.u.p.p;
 		    break;
 		case DILL_F:
-		    av_float(alist, ec->out_params[i].val.u.f.f);
+		    args[i] = &ffi_type_float;
+		    values[i] = &ec->out_params[i].val.u.f.f;
 		    break;
 		case DILL_D:
-		    av_double(alist, ec->out_params[i].val.u.d.d);
+		    args[i] = &ffi_type_double;
+		    values[i] = &ec->out_params[i].val.u.d.d;
 		    break;
 		case DILL_V:
 		    break;
 		}
 	    }
-	    av_call(alist);
+	    if (ffi_prep_cif(&cif, FFI_DEFAULT_ABI, 1,
+			     ret_type, args) == FFI_OK) {
+		ffi_call(&cif, func, ret_addr, values);
+	    }
 #endif
 	}
 	    break;
@@ -503,7 +563,27 @@ static void run_emulation(dill_exec_ctx ec)
 	    break;
 	case iclass_nop:
 	    break;
+	case iclass_compare:
+	{
+	    /* arith 3 operand integer insns */
+	    int r0 = ip->opnds.a3.src1;
+	    int r1 = ip->opnds.a3.src2;
+	    int d = ip->opnds.a3.dest;
+	    pused[0] = PREG(ec, r0);
+	    pused[1] = PREG(ec, r1);
+	    pdest = PREG(ec, d);
+	    pdest->u.i.i = emulate_compare(insn_code, pused[0], pused[1]);
+	    break;
+	}
+	case iclass_mark_label:
+	    break;
+	default:
+	    printf("Unhandled insn in emulator, %d\n", ip->class_code);
+	    break;
 	}
 	ip++;
     }
 }
+
+
+
