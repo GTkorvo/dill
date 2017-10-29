6958101b25a4cfe8ceb3e5691140709fb03c91b9
Greg Eisenhauer <eisen@cc.gatech.edu>
2017-10-29 16:31:57 -0400

    Kick version number for ppc64le support

--- version.c
+++ version.c
@@ -11,7 +11,7 @@
 #include <stdio.h>
 #include "config.h"
 
-static char *DILL_version = "DILL Version 2.1.20 rev. 26894  -- 2017-05-29 08:59:54 -0400 (Mon, 29 May 2017)\n";
+static char *DILL_version = "DILL Version 2.2.0 -- Sun Oct 29 16:31:12 EDT 2017\n";
 
 void DILLprint_version(){
     printf("%s",DILL_version);
