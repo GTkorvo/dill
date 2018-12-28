75e27efb16f1e0ce88982e0e4f5f972a00f5ce4e
Chuck Atkins <chuck.atkins@kitware.com>
2018-12-28 12:38:16 -0500

    Re-work the export of dependency information (i.e. ffi)

--- cmake/FindBinUtils.cmake
+++ cmake/FindBinUtils.cmake
@@ -1,11 +1,16 @@
 #
 #  BinUtils_FOUND - system has the BinUtils library
-#  BinUtils_INCLUDE_DIR - the BinUtils include directory
+#  BinUtils_INCLUDE_DIRS - the BinUtils include directory
 #  BinUtils_LIBRARIES - The libraries needed to use BinUtils
+#
+#  Targets:
+#    binutils::opcodes
+#    binutils::bfd
+#
 
+find_path(BinUtils_INCLUDE_DIR dis-asm.h)
 find_library(BinUtils_opcodes_LIBRARY opcodes)
 find_library(BinUtils_bfd_LIBRARY bfd)
-find_path(BinUtils_INCLUDE_DIR dis-asm.h)
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(BinUtils DEFAULT_MSG
@@ -14,4 +19,22 @@ find_package_handle_standard_args(BinUtils DEFAULT_MSG
 if(BinUtils_FOUND)
   set(BinUtils_INCLUDE_DIRS ${BinUtils_INCLUDE_DIR})
   set(BinUtils_LIBRARIES ${BinUtils_opcodes_LIBRARY} ${BinUtils_bfd_LIBRARY})
+
+  if(NOT TARGET binutils::bfd)
+    add_library(binutils::bfd UNKNOWN IMPORTED)
+    set_target_properties(binutils::bfd PROPERTIES
+      IMPORTED_LOCATION              ${BinUtils_bfd_LIBRARY}
+      INTERFACE_INCLUDE_DIRECTORIES  ${BinUtils_INCLUDE_DIR}
+    )
+  endif()
+
+  if(NOT TARGET binutils::opcodes)
+    add_library(binutils::opcodes UNKNOWN IMPORTED)
+    set_target_properties(binutils::opcodes PROPERTIES
+      IMPORTED_LOCATION              ${BinUtils_opcodes_LIBRARY}
+      INTERFACE_INCLUDE_DIRECTORIES  ${BinUtils_INCLUDE_DIR}
+      INTERFACE_LINK_LIBRARIES       binutils::bfd
+    )
+  endif()
 endif()
+
